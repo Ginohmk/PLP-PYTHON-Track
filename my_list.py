c1f7d3a1d39d2c4e2590eef1c510d59dac2e5335
ginohmk <michotall95@gmail.com>
2024-02-21 20:20:18 +0100

    efat: add my list assignment

---
+++ my_list.py
@@ -0,0 +1,16 @@
+my_list = [10,20,30,40]
+
+# INsert 15  at second position
+my_list[1]= 15
+
+# extend list
+my_list.extend([50,60,70])
+
+# Remove last elemet
+my_list.pop()
+
+# sort in asc order
+my_list.sort()
+
+# find and print index of 30
+print(my_list.index(30))
--- user_input.py
+++ user_input.py
@@ -5,3 +5,5 @@ age = str(input('Enter your age: '))
 location = str(input('Enter your location: '))
 
 print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
+
+
