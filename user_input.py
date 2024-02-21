341b9bffae1cced1e980775a00ea7699e71c9297
ginohmk <michotall95@gmail.com>
2024-02-21 19:50:32 +0100

    feat: add USer input file

---
+++ user_input.py
@@ -0,0 +1,7 @@
+
+# User input
+name = str(input('Enter your name: '))
+age = str(input('Enter your age: '))
+location = str(input('Enter your location: '))
+
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
